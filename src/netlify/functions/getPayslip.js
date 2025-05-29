1193a8bcba1d6dc44d3efb21462d8a6d8972ce88
kandukuri Sandesh <kandukurisandesh1998@gmail.com>
2025-05-29 18:19:34 +0100

    changed env name

--- src/netlify/functions/getPayslip.js
+++ src/netlify/functions/getPayslip.js
@@ -6,8 +6,8 @@ const REGION = 'us-east-1'
 
 const s3 = new AWS.S3({
     region:REGION,
-    accessKeyId:process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY
+    accessKeyId:process.env.ACCESS_KEY_ID,
+    secretAccessKey:process.env.SECRET_ACCESS_KEY
 })
 
 exports.handler = async function (event){
